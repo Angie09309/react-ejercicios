c9ae0f73c64bf3162fb4dddac8ce73a843589d3c
Angie Barreto <angiepbg13@gmail.com>
2026-01-06 15:18:19 -0500

    Cambiar título con useEffect según estado

--- src/components/BotonColor.jsx
+++ src/components/BotonColor.jsx
@@ -1,20 +1,29 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 
 const BotonColor = () => {
 
     const [activo, setActivo] = useState(false)
     const color = activo ? "pink" : "yellow"
 
+    const [text, setText] = useState("Botón inactivo")
+
+    useEffect(() => {
+        if (activo) {
+            setText("Botón activo")
+        } else {
+            setText("Botón inactivo")
+        }
+    }, [activo])
 
     return (
         <div>
+            <h1>{text}</h1>
+
             <button
                 style={{ backgroundColor: color }}
                 onClick={() => setActivo(prev => !prev)}>
                 {activo ? "Activo" : "Inactivo"}
-
             </button>
-
         </div>
     )
 }
